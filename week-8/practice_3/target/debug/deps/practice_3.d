5bcd8a129ecbbbfaae67b1ce16ce23b6b1a0bd90
Hussaini16-Lanks <philemon.michael-hussaini@pau.edu.ng>
2024-12-15 22:39:54 +0100

    (my eight commit)

---
+++ week-8/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\philh\Documents\P.Michael-HussainiCSC101\week-8\practice_3\target\debug\deps\practice_3.exe: src/main.rs
+
+C:\Users\philh\Documents\P.Michael-HussainiCSC101\week-8\practice_3\target\debug\deps\practice_3.d: src/main.rs
+
+src/main.rs:
---
+++ week-8/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\philh\Documents\P.Michael-HussainiCSC101\week-8\practice_4\target\debug\deps\practice_4.exe: src/main.rs
+
+C:\Users\philh\Documents\P.Michael-HussainiCSC101\week-8\practice_4\target\debug\deps\practice_4.d: src/main.rs
+
+src/main.rs:
